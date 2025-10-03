c42b84ac3b4c35e11329e76e64a53f3e42f7d052
Linh Le <s4091144@student.rmit.edu.au>
2025-10-03 18:15:59 +1000

    feature: sharing data btw ResumeViewer and ControlledForm

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,48 @@
-
+import { useState } from 'react'
 import './App.css'
 import { ControlledForm } from './components/ControlledForm'
 import ResumeViewer from './components/ResumeViewer'
 
 function App() {
+    const [formData, setFormData] = useState({
+      fullName: '',
+      email: '',
+      phoneNumber: ''
+    })
+
+    // const handleSubmit = (e) => {
+    //   e.preventDefault()
+    //   // Handle submit will decide what to do with data i.e sendData(). 
+    //   // Will printout form for now
+    //   console.log(formData)
+    // }
+
+    const handleChange = (e) => {
+
+      // https://dev.to/sidramaqbool/how-to-handle-forms-in-react-4jml
+      const { name, value } = e.target;
+      setFormData((prevData) => ({
+        ...prevData,  // Spread operator to take previous data
+        [name]: value,  // and dynamically assign name and value of inputs (Computed Property Names)
+      }))
+    }
+
+
+
 
   return (
     <>
 
 
-      <ControlledForm></ControlledForm>
-      <ResumeViewer></ResumeViewer>
+      <ControlledForm
+        formData={formData}
+        onChange={handleChange}
+      >
+
+      </ControlledForm>
+      <ResumeViewer
+        formData={formData}
+      ></ResumeViewer>
     </>
   )
 }
